3a2084e87ab2ed4aa1c8957405a318e572a35b09
AryanJJChympa <aryanjjchympa@gmail.com>
2024-10-05 01:08:49 +0530

    Add files via upload
    MY DSA JOURNEY (Not all the programs will be available here , Some of them have been solved online on Leetcode & G4G ...)!🚀

---
+++ bubbleSort.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int arr[1000];
+    int n;
+    cout<<"Enter the no. of elements in array: ";
+    cin>>n;
+    cout<<"Enter the elements of array : ";
+    for(int i=0; i<n ; i++){
+        cin>>arr[i];
+    }
+
+    for(int i=n-2; i>=0 ; i--){
+        for(int j=0; j<=i; j++){
+            if(arr[j]<arr[j+1]){
+                swap(arr[j],arr[j+1]);
+            }
+        }
+    }
+    cout<<"The array after bubble sort is: "<<endl;
+    for(int i=0; i<n; i++){
+        cout<<arr[i]<<" ";
+    }
+
+}
---
+++ reverseInteger.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int reverse(int x){
+    int ans=0,rem;
+    while(x!=0){
+        rem=x%10;
+        x/=10;
+        ans=ans*10+rem;
+    }
+    return ans;
+}
+
+int main(){
+    int a;
+    cin>>a;
+    int res=reverse(a);
+    cout<<res;
+}
