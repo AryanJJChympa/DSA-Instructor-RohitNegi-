1b7d6068e4b5fa06183417877765921db0b39438
Aryan Jain Chympa <aryanjjchympa@gmail.com>
2024-12-04 22:48:06 +0530

    Update bubbleSort.cpp

--- bubbleSort.cpp
+++ bubbleSort.cpp
@@ -9,7 +9,6 @@ int main(){
     for(int i=0; i<n ; i++){
         cin>>arr[i];
     }
-
     for(int i=n-2; i>=0 ; i--){
         for(int j=0; j<=i; j++){
             if(arr[j]<arr[j+1]){
@@ -21,5 +20,4 @@ int main(){
     for(int i=0; i<n; i++){
         cout<<arr[i]<<" ";
     }
-
 }
